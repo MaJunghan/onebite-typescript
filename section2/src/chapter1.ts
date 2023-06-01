272dd30155009c35cab5096dfddd8b0b26cd254b
MaJungHan <wjdgksak@naver.com>
2023-06-01 22:07:56 +0900

    원시타입과 리터럴타입

---
+++ section2/src/chapter1.ts
@@ -0,0 +1,28 @@
+// number
+let num1 = 123;
+let num2 = -123;
+let num3 = 0.123;
+let num4 = -0.123;
+let num5 = Infinity;
+let num6 = -Infinity;
+let num7 = NaN;
+
+// string
+let str1 = 'hello';
+let str2 = `hello`;
+let str3 = `hello ${name}`;
+
+// boolean
+let bool1 = true;
+let bool2 = false;
+
+// null
+let null1: null = null;
+
+// undefined
+let unde1: undefined = undefined;
+
+// let numA: number = null;
+
+// 리터럴 타입
+let numA: 10 = 10;
