f188e6b3a492fe3ae2c39923f2dd97d14ad24711
MaJungHan <wjdgksak@naver.com>
2024-10-07 15:57:30 +0900

    object

---
+++ 2회차/section2/src/chapter3.ts
@@ -0,0 +1,25 @@
+
+
+let user : {
+    id: number;
+    name: string;
+} = {
+    id: 1,
+    name: '마정한'
+}
+
+let dog : {
+    name: string;
+    color: string;
+}= {
+    name: '돌돌이',
+    color: 'brown'
+}
+
+let config : {
+    readonly apiKey: string
+} = {
+    apiKey: 'my api key'
+}
+
+// config.apiKey = 'tqweqw'
