521215fcf29b0613050e6f597ad6f24c40f04d90
MaJungHan <wjdgksak@naver.com>
2024-12-24 13:54:11 +0900

    section3 타입가드

---
+++ 2회차/section3/src/chapter7.ts
@@ -0,0 +1,27 @@
+/**
+ * 타입 좁히기
+ * 조건문 등을 이용해 넓은 타입에서 좁은 타입으로
+ */
+
+// value => number : toFixed
+// value => string : toUpperCase
+// value => Date : getTime
+// value => Person : name은 age살 입니다.
+
+type Person = {
+    name: string;
+    age: number;
+}
+
+function func(value: number | string | Date | null | Person) {
+    if(typeof value === 'number') {
+        console.log(value.toFixed())
+    } else if(typeof value === 'string') {
+        console.log(value.toUpperCase())
+    } else if(value instanceof Date) {
+        console.log(value.getTime())
+    } else if(value && 'age' in value) {
+        console.log()
+    }
+}
+
