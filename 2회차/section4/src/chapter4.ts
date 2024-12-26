f9a9c16cfccb2312ea05094b1973f26a6c743d40
MaJungHan <wjdgksak@naver.com>
2024-12-26 14:30:07 +0900

    section4 사용자 정의 타입가드

--- 2회차/section4/src/chapter4.ts
+++ 2회차/section4/src/chapter4.ts
@@ -1,25 +1,31 @@
 /**
- * 함수 오버로딩
- * 하나의 함수를 매개변수의 개수나 타입에 따라서 여러가지 버전으로 정의하는 방법
+ * 사용자 정의 타입 가드
  */
 
-// 버전들 -> 오버로드 시그니쳐
-function func(a : number):void;
-function func(a: number, B: number, c: number):void;
-
-
-// 실제 구현부 -> 구현 시그니쳐
-function func(a:number, b?: number, c?: number) {
-    if(typeof b === 'number' && typeof c === 'number') {
-        console.log(a+b+c)
-    } else {
-        console.log(a * 20)
-    }
+type Dog = {
+    name: string;
+    isBark: boolean;
 }
 
+type Cat = {
+    name: string;
+    isScratch: boolean;
+}
 
+type Animal = Dog | Cat;
 
-// func()
-func(1)
-// func(1,2)
-func(1,2,3)
+function isDog(animal: Animal): animal is Dog {
+    return (animal as Dog).isBark !== undefined;
+}
+
+function isCat(animal: Animal): animal is Cat {
+    return (animal as Cat).isScratch !== undefined;
+}
+
+function warning(animal: Animal) {
+    if(isDog(animal)) {
+        console.log(animal)
+    } else if(isCat(animal)) {
+        console.log(animal)
+    }
+}
